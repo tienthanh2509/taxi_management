2b58969668f072561e12525d92cf15d1ca1d9131
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2015-11-11 21:09:19 +0700

    Tạo trang bảng giá dịch vụ

---
+++ application/controllers/Services.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ *  Chương trình quản lý Taxi
+ *  Thiết kế bởi nhóm 1 lớp D13HT01
+ *  Bao gồm các thành viên
+ *  Hoàng Huy, Thái Sơn, Tiến Thành, Thanh Thúy, Thanh Vân
+ */
+
+/**
+ * Bảng giá dịch vụ
+ *
+ * @author Phạm Tiến Thành <tienthanh.dqc@gmail.com>
+ */
+class Services extends CI_D13HT01_Guest {
+
+	public function index()
+	{
+		$this->render('services');
+	}
+
+}
