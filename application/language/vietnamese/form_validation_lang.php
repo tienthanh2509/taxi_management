5169907975c8639a01aaffae83229bfffdeaddca
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2015-11-11 11:09:41 +0700

    Thêm gói tiếng Việt
    Gói tiếng Việt cho các thông báo CodeIgniter.

---
+++ application/language/vietnamese/form_validation_lang.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['form_validation_required']		= '{field} không được để trống.';
+$lang['form_validation_isset']			= '{field} phải có một giá trị.';
+$lang['form_validation_valid_email']		= '{field} phải là một địa chỉ Email hợp lệ.';
+$lang['form_validation_valid_emails']		= '{field} Các trường phải là địa chỉ Email hợp lệ.';
+$lang['form_validation_valid_url']		= '{field} phải là một URL hợp lệ.';
+$lang['form_validation_valid_ip']		= '{field} phải là một địa chỉ IP hợp lệ.';
+$lang['form_validation_min_length']		= '{field} phải là ít nhât {param} ký tự.';
+$lang['form_validation_max_length']		= '{field} không được vượt quá {param} ký tự.';
+$lang['form_validation_exact_length']		= '{field} phải đúng {param} ký tự.';
+$lang['form_validation_alpha']			= '{field} Chỉ sử dụng các ký tự là CHỮ.';
+$lang['form_validation_alpha_numeric']		= '{field} Chỉ sử dụng các ký tự là SỐ.';
+$lang['form_validation_alpha_numeric_spaces']	= '{field} Chỉ sử dụng các ký tự là SỐ và Khoảng Trắng.';
+$lang['form_validation_alpha_dash']		= '{field} Chỉ sử dụng các ký tự là SỐ, Gạch Dưới, Gạch Ngang.';
+$lang['form_validation_numeric']		= '{field} Chỉ sử dụng các ký tự là SỐ.';
+$lang['form_validation_is_numeric']		= '{field} phải chứa các ký tự là SỐ.';
+$lang['form_validation_integer']		= '{field} phải chứa một số Nguyên.';
+$lang['form_validation_regex_match']		= '{field} sai định dạng.';
+$lang['form_validation_matches']		= '{field} không khớp với {param}.';
+$lang['form_validation_differs']		= '{field} phải khác với {param} .';
+$lang['form_validation_is_unique'] 		= '{field} đã tồn tại.';
+$lang['form_validation_is_natural']		= '{field} chỉ chấp nhận các số.';
+$lang['form_validation_is_natural_no_zero']	= '{field} dữ liệu nhập vào phải chứa số và lớn hơn không.';
+$lang['form_validation_decimal']		= '{field} Dữ liệu nhập vào phải là số thập phân.';
+$lang['form_validation_less_than']		= '{field} phải ít hơn {param}.';
+$lang['form_validation_less_than_equal_to']	= '{field} phải ít hơn hoặc bằng {param}.';
+$lang['form_validation_greater_than']		= '{field} phải lơn hơn {param}.';
+$lang['form_validation_greater_than_equal_to']	= '{field} phải lơn hơn hoặc bằng {param}.';
+$lang['form_validation_error_message_not_set']	= 'Không thể xác định thông báo lỗi với trường {field}.';
+$lang['form_validation_in_list']		= '{field} phải là một trong: {param}.';
