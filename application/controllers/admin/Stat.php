26d7281d1b0204c888fcf5cdd45e20400c432570
Phạm Tiến Thành <tienthanh.dqc@gmail.com>
2015-11-17 08:55:01 +0700

    Chỉnh sửa nhỏ

---
+++ application/controllers/admin/Stat.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ *  Chương trình quản lý Taxi
+ *  Thiết kế bởi nhóm 1 lớp D13HT01
+ *  Bao gồm các thành viên
+ *  Hoàng Huy, Thái Sơn, Tiến Thành, Thanh Thúy, Thanh Vân
+ */
+
+/**
+ * Description of Stat
+ *
+ * @author Phạm Tiến Thành <tienthanh.dqc@gmail.com>
+ */
+class Stat extends CI_D13HT01 {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		
+	}
+}
